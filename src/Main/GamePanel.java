aa7afd0fae7c236fad14ef762ec41a0c5ddabaf1
boydpelley <boydjrpelley@gmail.com>
2024-04-09 20:06:29 -0300

    Can now handle keyboard input, a box moves around the screen. Also created a game loop that runs in 60 FPS.

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -16,13 +16,25 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 960 pixels
     final int screenHeight = tileSize * maxScreenRow; // 720 pixels
 
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.pink);
         this.setDoubleBuffered(true);
+
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -34,9 +46,71 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000 / FPS; // 0.016666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        long timer = System.nanoTime();
+        int drawCount = 0;
+
         while (gameThread != null) {
 
-            System.out.println("The game loop is running");
+            update();
+
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime / 1000000;
+
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+                drawCount++;
+
+                if (System.nanoTime() - timer > 1000000000) { // If a second has passed
+                    System.out.println("FPS: " + drawCount);
+                    drawCount = 0;
+                    timer = System.nanoTime();
+                }
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
+
+    public void update() {
+
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        }
+        else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        }
+
+        if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
     }
 }

98326bf68be45b2940256d186014c90b84b68eca
boydpelley <boydjrpelley@gmail.com>
2024-04-09 19:30:35 -0300

    Start game loop.

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -3,7 +3,7 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16 x 16 tile
@@ -16,10 +16,27 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 960 pixels
     final int screenHeight = tileSize * maxScreenRow; // 720 pixels
 
+    Thread gameThread;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.pink);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (gameThread != null) {
+
+            System.out.println("The game loop is running");
+        }
     }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -17,5 +17,7 @@ public class Main {
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }

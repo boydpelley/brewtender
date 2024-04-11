80f0f6c9ec7d984d8aed69cda28d395745ccf1bb
boydpelley <boydjrpelley@gmail.com>
2024-04-11 00:33:54 -0300

    Starting to add tiles. Create Tile and TileManager class

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,6 +1,7 @@
 package Main;
 
 import Entity.Player;
+import Tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -21,14 +22,11 @@ public class GamePanel extends JPanel implements Runnable {
     // FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
 
-    // Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
 
@@ -99,6 +97,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D)g;
 
+        tileM.draw(g2);
         player.draw(g2);
 
         g2.dispose();
---
+++ src/Tile/TileManager.java
@@ -0,0 +1,34 @@
+package Tile;
+
+import Main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+public class TileManager {
+
+    GamePanel gp;
+    Tile[] tile;
+
+
+    public TileManager(GamePanel gp) {
+        this.gp = gp;
+        tile = new Tile[10];
+        getTileImage();
+    }
+
+    public void getTileImage() {
+        try {
+
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("tiles/grass.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.drawImage(tile[0].image, 0, 0, gp.tileSize, gp.tileSize, null);
+    }
+}

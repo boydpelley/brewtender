7a7a04fabc8d0ad8babe4e2e1033476ba32dc8f5
boydpelley <boydjrpelley@gmail.com>
2024-04-17 00:09:27 -0300

    Moved some code to super class

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -35,7 +35,22 @@ public class Entity {
 
     }
 
-    public void speak() {}
+    public void speak() {
+
+        if (dialogues[dialogueIndex] == null) {
+            dialogueIndex = 0;
+        }
+
+        gp.ui.currentDialogue = dialogues[dialogueIndex];
+        dialogueIndex++;
+
+        switch(gp.player.direction) {
+            case "up": direction = "down"; break;
+            case "down": direction = "up"; break;
+            case "left": direction = "right"; break;
+            case "right": direction = "left"; break;
+        }
+    }
 
     public void update() {
 
--- src/Entity/NPC_Cop.java
+++ src/Entity/NPC_Cop.java
@@ -69,19 +69,6 @@ public class NPC_Cop extends Entity {
     }
 
     public void speak() {
-
-        if (dialogues[dialogueIndex] == null) {
-            dialogueIndex = 0;
-        }
-
-        gp.ui.currentDialogue = dialogues[dialogueIndex];
-        dialogueIndex++;
-
-        switch(gp.player.direction) {
-            case "up": direction = "down"; break;
-            case "down": direction = "up"; break;
-            case "left": direction = "right"; break;
-            case "right": direction = "left"; break;
-        }
+        super.speak();
     }
 }

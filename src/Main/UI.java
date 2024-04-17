1e39fb3d6ab099de3720ca512b8e5ed144e97796
boydpelley <boydjrpelley@gmail.com>
2024-04-17 11:37:09 -0300

    Add pixelated font for text!

--- src/Main/UI.java
+++ src/Main/UI.java
@@ -3,6 +3,8 @@ package Main;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 import Object.OBJ_CrabApples;
 
@@ -10,7 +12,7 @@ public class UI {
 
     GamePanel gp;
     Graphics2D g2;
-    Font arial_40;
+    Font retroGaming;
     BufferedImage crabApplesImage;
     public boolean messageOn = false;
     public String message = "";
@@ -20,7 +22,15 @@ public class UI {
     public UI(GamePanel gp) {
         this.gp = gp;
 
-        arial_40 = new Font("Arial", Font.PLAIN, 40);
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream("font/Retro_Gaming.ttf");
+            retroGaming = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         OBJ_CrabApples crabApples = new OBJ_CrabApples(gp);
         crabApplesImage = crabApples.image;
     }
@@ -33,7 +43,7 @@ public class UI {
     public void draw(Graphics2D g2) {
         this.g2 = g2;
 
-        g2.setFont(arial_40);
+        g2.setFont(retroGaming);
         g2.setColor(Color.WHITE);
         g2.drawImage(crabApplesImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
         g2.drawString(": " + gp.player.hasCrabApples, 74, 65);
@@ -84,7 +94,7 @@ public class UI {
 
         drawSubWindow(x, y, width, height);
 
-        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 32F));
+        g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 22F));
         x += gp.tileSize;
         y += gp.tileSize;
 
